d4ff1d317e0ddfa6ecdf20632061c79076ccc6b6
Soojung Kwak <gsj2464@gmail.com>
2020-07-16 15:49:39 -0400

    modified

--- crmDjango/settings.py
+++ crmDjango/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,4 +142,3 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '*********'
 EMAIL_HOST_PASSWORD = '*********'
 
-django_heroku.settings(locals())
